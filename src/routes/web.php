af923ede17edc620a5f3aea126dfd764d9c61cdf
Kano.M <itspapari888@gmail.com>
2024-04-09 03:23:13 +0900

    打刻ページの表示

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TimecardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,3 +24,5 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::get('/', [TimecardController::class, 'index'])->middleware('auth')->name('dashboard');
