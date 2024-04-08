f9c9e10f504b7f55a269257c623da4e69ccaf287
Kano.M <itspapari888@gmail.com>
2024-04-09 02:27:00 +0900

    ログイン機能の実装

--- src/app/Http/Controllers/AuthController.php
+++ src/app/Http/Controllers/AuthController.php
@@ -29,4 +29,17 @@ class AuthController extends Controller
     public function showLoginForm() {
         return view('auth.login');
     }
+
+    public function login(Request $request) {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            return redirect()->intended('/');
+        }
+
+        return back()->withErrors(['email' => '提供された認証情報が記録と一致しません。']);
+    }
 }
