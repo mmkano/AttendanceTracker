5d8661413873b1360fd6fc264ccad59aadc868af
Kano.M <itspapari888@gmail.com>
2024-04-09 01:50:38 +0900

    会員登録の実装

---
+++ src/app/Http/Controllers/AuthController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use App\Http\Requests\RegisterRequest;
+
+class AuthController extends Controller
+{
+    public function showRegisterForm() {
+        return view('auth.register');
+    }
+
+    public function register(RegisterRequest $request) {
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        Auth::login($user);
+
+        return view('auth.login');
+    }
+}
