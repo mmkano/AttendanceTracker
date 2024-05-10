d9626beab41135eadc39bc8fc81a911c2f73daf0
Kano.M <itspapari888@gmail.com>
2024-05-11 00:48:32 +0900

    fix:3月分のデータを4月から現在までのものに変更

--- src/database/seeders/ComprehensiveSeeder.php
+++ src/database/seeders/ComprehensiveSeeder.php
@@ -22,56 +22,64 @@ class ComprehensiveSeeder extends Seeder
 
         $userCount = 105;
         User::factory($userCount)->create()->each(function ($user) use ($faker) {
-            for ($day = 1; $day <= 31; $day++) {
-                $date = sprintf('2024-03-%02d', $day);
-                $weekDay = (new \DateTime($date))->format('N');
-                $isHoliday = $this->isHoliday($date);
-                $defaultTime = $date . ' 00:00:00';
-                $startTime = $endTime = $defaultTime;
+        $startDate = Carbon::create(2024, 4, 1);
+        $endDate = Carbon::today();
 
-                if (!($weekDay >= 6 || $isHoliday)) {
-                    $startTime = $faker->dateTimeBetween($date . ' 08:00:00', $date . ' 16:00:00')->format('Y-m-d H:i:s');
-                    $endTime = $faker->dateTimeBetween($date . ' 17:00:00', $date . ' 23:59:59')->format('Y-m-d H:i:s');
-                }
+        while ($startDate->lte($endDate)) {
+            $date = $startDate->toDateString();
+            $weekDay = $startDate->format('N');
+            $isHoliday = $this->isHoliday($date);
+            $defaultTime = $date . ' 00:00:00';
+            $startTime = $endTime = $defaultTime;
 
-                $attendance = Attendance::factory()->create([
-                    'user_id' => $user->id,
-                    'date' => $date,
-                    'start_time' => $startTime,
-                    'end_time' => $endTime,
-                ]);
+            if (!($weekDay >= 6 || $isHoliday)) {
+                $startTime = $faker->dateTimeBetween($date . ' 08:00:00', $date . ' 16:00:00')->format('Y-m-d H:i:s');
+                $endTime = $faker->dateTimeBetween($date . ' 17:00:00', $date . ' 23:59:59')->format('Y-m-d H:i:s');
+            }
 
-                if (!($weekDay >= 6 || $isHoliday)) {
-                    $totalWorkMinutes = Carbon::parse($endTime)->diffInMinutes(Carbon::parse($startTime));
-                    $totalBreakMinutes = 0;
-                    $breakTimeCount = random_int(1, 3);
+            $attendance = Attendance::factory()->create([
+                'user_id' => $user->id,
+                'date' => $date,
+                'start_time' => $startTime,
+                'end_time' => $endTime,
+            ]);
 
-                    for ($i = 0; $i < $breakTimeCount; $i++) {
-                        $breakStartTime = Carbon::instance($faker->dateTimeBetween($startTime, $endTime));
-                        $breakEndTime = Carbon::instance(clone $breakStartTime)->modify('+'.rand(5, min(60, $totalWorkMinutes - $totalBreakMinutes)).' minutes');
-                        $breakMinutes = $breakEndTime->diffInMinutes($breakStartTime);
+            if (!($weekDay >= 6 || $isHoliday)) {
+                $totalWorkMinutes = Carbon::parse($endTime)->diffInMinutes(Carbon::parse($startTime));
+                $totalBreakMinutes = 0;
+                $breakTimeCount = random_int(1, 3);
 
-                        if ($totalBreakMinutes + $breakMinutes > $totalWorkMinutes || $totalBreakMinutes + $breakMinutes > 120) {
-                            break;
-                        }
+                for ($i = 0; $i < $breakTimeCount; $i++) {
+                    $breakStartTime = Carbon::instance($faker->dateTimeBetween($startTime, $endTime));
+                    $breakEndTime = Carbon::instance(clone $breakStartTime)->modify('+'.rand(5, min(60, $totalWorkMinutes - $totalBreakMinutes)).' minutes');
+                    $breakMinutes = $breakEndTime->diffInMinutes($breakStartTime);
 
-                        $totalBreakMinutes += $breakMinutes;
-
-                        BreakTime::create([
-                            'attendance_id' => $attendance->id,
-                            'break_start_time' => $breakStartTime->format('Y-m-d H:i:s'),
-                            'break_end_time' => $breakEndTime->format('Y-m-d H:i:s'),
-                        ]);
+                    if ($totalBreakMinutes + $breakMinutes > $totalWorkMinutes || $totalBreakMinutes + $breakMinutes > 120) {
+                        break;
                     }
+
+                    $totalBreakMinutes += $breakMinutes;
+
+                    BreakTime::create([
+                        'attendance_id' => $attendance->id,
+                        'break_start_time' => $breakStartTime->format('Y-m-d H:i:s'),
+                        'break_end_time' => $breakEndTime->format('Y-m-d H:i:s'),
+                    ]);
                 }
             }
-        });
+            $startDate->addDay();
+        }
+    });
     }
 
     private function isHoliday($date)
     {
         $holidays = [
-            '2024-03-20',
+            '2024-04-29',
+            '2024-05-03',
+            '2024-05-04',
+            '2024-05-05',
+            '2024-05-06',
         ];
 
         return in_array($date, $holidays);
